da8d6b597fb08d63a32262f6f10b4624b0064572
Mark Luy <tamiya308@protonmail.com>
2026-09-23 17:59:16 +0800

    various updates to the codePipeline cdk project.

--- lib/ci-cd-aws-pipeline-demo-stack.ts
+++ lib/ci-cd-aws-pipeline-demo-stack.ts
@@ -1,5 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { ComputeType, LinuxBuildImage } from 'aws-cdk-lib/aws-codebuild';
 import { CodePipeline, CodePipelineSource, ShellStep, Step } from 'aws-cdk-lib/pipelines';
 import { ManualApprovalStep } from 'aws-cdk-lib/pipelines';
 import { MyPipelineAppStage } from './stage';
@@ -10,23 +11,34 @@ export class CiCdAwsPipelineDemoStack extends cdk.Stack {
 
     const pipeline = new CodePipeline(this, 'Pipeline', {
       pipelineName: 'TestPipeline',
+      codeBuildDefaults: {
+        buildEnvironment: {
+          computeType: ComputeType.SMALL,
+          buildImage: LinuxBuildImage.STANDARD_5_0,
+        },
+      },
+      
       synth: new ShellStep('Synth', {
-        input: CodePipelineSource.gitHub('tamiya308/aws-cicd', 'main'),
+        input: CodePipelineSource.connection('tamiya308/aws-cicd', 'main', {
+          connectionArn: 'arn:aws:codestar-connections:ap-southeast-2:610433738771:connection/63d86147-f0bc-4c32-802a-a6716527ff6a',
+        }),
         commands: ['npm ci', 
                    'npm run build', 
                    'npx cdk synth']
       })
     });
 
-
-
-    const testingStage = pipeline.addStage(new MyPipelineAppStage(this, "test", {
+    const devStage = pipeline.addStage(new MyPipelineAppStage(this, "dev", {
       env: { account: "610433738771", region: "ap-southeast-2" }
     }));
 
+    devStage.addPost(new ManualApprovalStep('Manual approval before uat'));
 
-    testingStage.addPre(new ShellStep("Run Unit Tests", { commands: ['npm install', 'npm test'] }));
-    testingStage.addPost(new ManualApprovalStep('Manual approval before production'));
+    const uatStage = pipeline.addStage(new MyPipelineAppStage(this, "uat", {
+      env: { account: "610433738771", region: "ap-southeast-2" }
+    }));
+    
+    uatStage.addPost(new ManualApprovalStep('Manual approval before production'));
 
     const prodStage = pipeline.addStage(new MyPipelineAppStage(this, "prod", {
       env: { account: "610433738771", region: "ap-southeast-2" }
